8cf5d3705b2b233a094aa21586612d4243a05829
Carter <mega_narwhal@yahoo.com>
2020-07-12 09:24:22 -0400

    Properly handle flat worlds

--- src/main/java/fakedimension/Handler.java
+++ src/main/java/fakedimension/Handler.java
@@ -5,10 +5,7 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.utility.MinecraftReflection;
 import com.comphenix.protocol.wrappers.EnumWrappers.NativeGameMode;
-import net.minecraft.server.v1_16_R1.DimensionManager;
-import net.minecraft.server.v1_16_R1.PacketPlayOutRespawn;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldType;
@@ -25,34 +22,34 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class Handler implements Listener {
+public class Handler implements Listener{
 
 	private final Config config;
 
-	public Handler(Config config) {
+	public Handler(Config config){
 		this.config = config;
 	}
 
 	protected final Set<UUID> switchedDimension = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
-	public void start(FakeDimension plugin) {
-		Bukkit.getPluginManager().registerEvents(this, plugin);
+	public void start(FakeDimension plugin){
+		Bukkit.getPluginManager().registerEvents(this,plugin);
 
 		// fake dimension on login
-		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(PacketAdapter.params(plugin, PacketType.Play.Server.LOGIN)) {
+		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(PacketAdapter.params(plugin,PacketType.Play.Server.LOGIN)){
 
 			@Override
-			public void onPacketSending(PacketEvent event) {
+			public void onPacketSending(PacketEvent event){
 				config.getDimension(event.getPlayer().getWorld().getName())
-				.ifPresent(dimension -> event.getPacket().getModifier().write(7, dimension.getKeyDimension()));
+						.ifPresent(dimension -> event.getPacket().getModifier().write(7,dimension.getKeyDimension()));
 			}
 
 		});
 
 		// track dimension on dimension switch (we have to do this because dimension switch is sent before player actually enters world)
-		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(PacketAdapter.params(plugin, PacketType.Play.Server.RESPAWN)) {
+		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(PacketAdapter.params(plugin,PacketType.Play.Server.RESPAWN)){
 			@Override
-			public void onPacketSending(PacketEvent event) {
+			public void onPacketSending(PacketEvent event){
 				switchedDimension.add(event.getPlayer().getUniqueId());
 			}
 		});
@@ -60,51 +57,55 @@ public class Handler implements Listener {
 		/* actually fake dimension on first position packet <br>
 		/* we switch dimensions using middle dimension twice even we don't have fake dimension in new world
 		/* because player might have respawned in the same dimension after switch from faked to non faked dimension*/
-		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(PacketAdapter.params(plugin, PacketType.Play.Server.POSITION)) {
+		ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(PacketAdapter.params(plugin,PacketType.Play.Server.POSITION)){
 
-			PacketContainer createDimensionSwitch(Player player, World world, Dimension targetDimension) {
+			PacketContainer createDimensionSwitch(Player player,World world,Dimension targetDimension){
 				PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.RESPAWN);
-				packet.getModifier().write(0, targetDimension.getKeyDimension());
-				packet.getGameModes().write(0, NativeGameMode.fromBukkit(player.getGameMode()));
-				packet.getModifier().write(1, targetDimension.getKeyWorld());
+				packet.getModifier().write(0,targetDimension.getKeyDimension());
+				packet.getGameModes().write(0,NativeGameMode.fromBukkit(player.getGameMode()));
+				packet.getModifier().write(1,targetDimension.getKeyWorld());
+				System.out.println(world.getWorldType());
+				packet.getBooleans().write(1,world.getWorldType() == WorldType.FLAT);
 				return packet;
 			}
 
-			Dimension getMiddleDimension(Dimension dimension) {
+			Dimension getMiddleDimension(Dimension dimension){
 				return dimension == Dimension.NORMAL ? Dimension.NETHER : Dimension.NORMAL;
 			}
 
-			void sendPacketNow(Player player, PacketContainer packet) {
-				try {
-					ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet, false);
-				} catch (InvocationTargetException e) {
+			void sendPacketNow(Player player,PacketContainer packet){
+				try{
+					ProtocolLibrary.getProtocolManager().sendServerPacket(player,packet,false);
+				}catch(InvocationTargetException e){
 					player.kickPlayer("Fake dimension failure");
 					e.printStackTrace();
 				}
 			}
 
 			@Override
-			public void onPacketSending(PacketEvent event) {
+			public void onPacketSending(PacketEvent event){
 				Player player = event.getPlayer();
 
-				if (!switchedDimension.remove(player.getUniqueId())) {
+				if(!switchedDimension.remove(player.getUniqueId())){
 					return;
 				}
 
 				World world = player.getWorld();
 				Optional<Dimension> optDimension = config.getDimension(world.getName());
 
-				Dimension targetDimension = optDimension.orElseGet(() -> Dimension.getByBukkit(world.getEnvironment()));
+				Dimension targetDimension = optDimension.orElse(null);
 
-				sendPacketNow(player, createDimensionSwitch(player, world, getMiddleDimension(targetDimension)));
-				sendPacketNow(player, createDimensionSwitch(player, world, targetDimension));
+				if(targetDimension == null) return;
+
+				sendPacketNow(player,createDimensionSwitch(player,world,getMiddleDimension(targetDimension)));
+				sendPacketNow(player,createDimensionSwitch(player,world,targetDimension));
 			}
 
 		});
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onQuit(PlayerQuitEvent event) {
+	public void onQuit(PlayerQuitEvent event){
 		switchedDimension.remove(event.getPlayer().getUniqueId());
 	}
 
